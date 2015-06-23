ba46c70fd65f35c7f60be6c06e85028a805cfd65
David Wickes <dave.wickes@gmail.com>
2015-06-23 17:48:13 +0100

    QuickUnion completed in Java

--- code/java/src/main/java/QuickUnionUF.java
+++ code/java/src/main/java/QuickUnionUF.java
@@ -1,3 +1,21 @@
 public class QuickUnionUF {
 
+  private int[] id;
+
+  public QuickUnionUF(int N) {
+   id = new int[N];
+  }
+
+  public void union(int first, int second) {
+    id[first] = id[second];
+  }
+
+  public boolean connected(int first, int second) {
+    return root(first) == root(second);
+  }
+
+  private int root(int index) {
+    if (id[index] == index) return index;
+    return root(id[index]);
+  }
 }
