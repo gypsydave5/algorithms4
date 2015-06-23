d11449ac326ed1cf64d9cd6810d810d9f87ab2cd
David Wickes <dave.wickes@gmail.com>
2015-06-23 14:05:00 +0100

    Java version of QuickFind - start of QuickUnion

---
+++ code/java/src/main/java/QuickFindUF.java
@@ -0,0 +1,23 @@
+public class QuickFindUF {
+
+  private int[] id;
+
+  public QuickFindUF(int N) {
+    id = new int[N];
+    for (int i = 0; i < N; i++) {
+      id[i] = i;
+    }
+  }
+
+  public boolean connected(int first, int second) {
+    return id[first] == id[second];
+  }
+
+  public void union(int first, int second) {
+    int firstId = id[first];
+    int secondId = id[second];
+    for (int i = 0; i < id.length; i++) {
+      if (id[i] == firstId) id[i] = secondId;
+    }
+  }
+}
